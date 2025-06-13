d52183fb540aae33c57fd6a958dad2fb35f64c71
Steven van Blerk <stevenvanblerkrsa@gmail.com>
2025-06-13 20:34:02 +0200

    chore: added vite-plugin-svgr to handle dynamic SVG usage

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import svgr from "vite-plugin-svgr"; // transforms SVGs into react components
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       src: path.resolve(__dirname, "./src"),
